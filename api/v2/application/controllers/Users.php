62ce519f75a0d6413da521c77dd6614eed4e69c9
TijaniDevIt <tijanidevit@yahoo.com>
2021-07-02 19:21:03 +0100

    BikesController

--- api/v2/application/controllers/Users.php
+++ api/v2/application/controllers/Users.php
@@ -132,196 +132,22 @@ class Users extends REST_Controller
             'user_id' => $this->input->post('user_id'),
             'bike_id' => $this->input->post('bike_id'),
         ];
-        
-        if ($this->users_model->check_user_booking($data)) {
-            return $this->response([
-                'status' => "error",
-                'message' => "user already added this booking.",
-                'status_code' => $this->status_code['conflict'],
-            ], $this->status_code['conflict']);
-        }
+
         $booking = $this->users_model->add_user_booking($data);
         if (!$booking) {
             return $this->response([
                 'status' => "error",
-                'message' => "user booking not added.",
+                'message' => "Booking not complete.",
                 'status_code' => $this->status_code['internalServerError'],
             ], $this->status_code['internalServerError']);
         }
 
         return $this->response([
             'status' => "success",
-            'message' => "user booking added successfully.",
+            'message' => "You just booked a bike successfully.",
             'status_code' => $this->status_code['created'],
             'data' => $booking
         ], $this->status_code['created']);
-    }
-
-    function questions_get($user_id)
-    {
-        $user_questions = $this->users_model->get_user_questions($user_id);
-        if ($user_questions == null) {
-            return $this->response([
-                'status' => "error",
-                'message' => "user has no question added.",
-                'status_code' => $this->status_code['ok'],
-            ], $this->status_code['ok']);
-        }
-        return $this->response([
-            'status' => "success",
-            'message' => "user questions fetched successfully.",
-            'status_code' => $this->status_code['ok'],
-            'data' => $user_questions
-        ], $this->status_code['ok']);
-
-    }
-
-    function answers_get($user_id)
-    {
-        $user_answers = $this->users_model->get_user_answers($user_id);
-        if ($user_answers == null) {
-            return $this->response([
-                'status' => "error",
-                'message' => "user has no answer added.",
-                'status_code' => $this->status_code['ok'],
-            ], $this->status_code['ok']);
-        }
-        return $this->response([
-            'status' => "success",
-            'message' => "user answers fetched successfully.",
-            'status_code' => $this->status_code['ok'],
-            'data' => $user_answers
-        ], $this->status_code['ok']);
-
-    }
-
-    // function plans_get($user_id = '')
-    // {
-    //     if ($user_id) {
-    //         $user_plans = $this->users_model->get_user_plans($user_id);
-    //         if ($user_plans == null) {
-    //             return $this->response([
-    //                 'status' => "error",
-    //                 'message' => "user has no plan added.",
-    //                 'status_code' => $this->status_code['ok'],
-    //             ], $this->status_code['ok']);
-    //         }
-    //         return $this->response([
-    //             'status' => "success",
-    //             'message' => "user plans fetched successfully.",
-    //             'status_code' => $this->status_code['ok'],
-    //             'data' => $user_plans
-    //         ], $this->status_code['ok']);
-    //     }
-    //     else{
-    //         $user_plans = $this->users_model->get_users_plans($user_id);
-    //         if ($user_plans == null) {
-    //             return $this->response([
-    //                 'status' => "error",
-    //                 'message' => "users have no plans added.",
-    //                 'status_code' => $this->status_code['ok'],
-    //             ], $this->status_code['ok']);
-    //         }
-    //         return $this->response([
-    //             'status' => "success",
-    //             'message' => "users plans fetched successfully.",
-    //             'status_code' => $this->status_code['ok'],
-    //             'data' => $user_plans
-    //         ], $this->status_code['ok']);
-    //     }
-    // }
-
-    // function plans_post()
-    // {
-    //     $this->form_validation->set_rules('user_id', 'user ID', 'required');
-    //     $this->form_validation->set_rules('plan_id', 'plan_id', 'required');
-    //     $this->form_validation->set_rules('points', 'points', 'required');
-    //     if ($this->form_validation->run() === FALSE) {
-    //         return $this->response([
-    //             'status' => "failed",
-    //             'message' => "All inputs are required.",
-    //             'status_code' => $this->status_code['badRequest'],
-    //             'data' => []
-    //         ], $this->status_code['badRequest']);
-    //     }
-
-    //     $data = [
-    //         'user_id' => $this->input->post('user_id'),
-    //         'plan_id' => $this->input->post('plan_id'),
-    //         'points' => $this->input->post('points'),
-    //     ];
-
-
-    //     $role_id = $this->fn_model->get_user_role_id('user');
-    //     $user = $this->fn_model->get_user_via_user_id($data['user_id']);
-
-    //     if ($user['role_id'] !== $role_id) {
-    //         return $this->response([
-    //             'status' => "error",
-    //             'message' => "User not a user.",
-    //             'status_code' => $this->status_code['unauthorized'],
-    //         ], $this->status_code['unauthorized']);
-    //     }
-
-
-    //     if (! $this->fn_model->get_user_plan($data['plan_id'])) {
-    //         return $this->response([
-    //             'status' => "error",
-    //             'message' => "plan does not exist.",
-    //             'status_code' => $this->status_code['ok'],
-    //         ], $this->status_code['ok']);
-    //     }
-
-    //     $plan = $this->users_model->add_user_plan($data);
-    //     if (!$plan) {
-    //         return $this->response([
-    //             'status' => "error",
-    //             'message' => "user plan not added.",
-    //             'status_code' => $this->status_code['internalServerError'],
-    //         ], $this->status_code['internalServerError']);
-    //     }
-
-    //     $old_points = $this->fn_model->get_user_points($data['user_id']);
-    //     $new_points = $data['points'];
-
-    //     $points = $old_points + $new_points;
-    //     $update_data = ['user_id' => $data['user_id'], 'points'=> $points];
-
-    //     $plan['new_points'] = $this->fn_model->update_user_points($update_data);
-
-    //     return $this->response([
-    //         'status' => "success",
-    //         'message' => "user plan added successfully.",
-    //         'status_code' => $this->status_code['created'],
-    //         'data' => $plan
-    //     ], $this->status_code['created']);
-    // }
-
-    function payments_get($user_id)
-    {
-        if ($user_id) {
-            $user_payments = $this->users_model->get_user_payments($user_id);
-            if ($user_payments == null) {
-                return $this->response([
-                    'status' => "error",
-                    'message' => "user has no payment added.",
-                    'status_code' => $this->status_code['ok'],
-                ], $this->status_code['ok']);
-            }
-            return $this->response([
-                'status' => "success",
-                'message' => "user payments fetched successfully.",
-                'status_code' => $this->status_code['ok'],
-                'data' => $user_payments
-            ], $this->status_code['ok']);
-        }
-        else{            
-            return $this->response([
-                'status' => "error",
-                'message' => "user ID required.",
-                'status_code' => $this->status_code['badRequest'],
-            ], $this->status_code['badRequest']);
-        }
     }
 
 }
--- api/v2/application/models/Users_model.php
+++ api/v2/application/models/Users_model.php
@@ -90,266 +90,27 @@ class users_model extends CI_Model
         };
     }
 
-    public function get_last_user_id()
-    {
-        $users = $this->db->select('id')->order_by('id', "desc")->limit(1)->get('users')->row();
-        return ($users->id + 1);
-    }
-
-    private function _get_user_details($friconn_id)
-    {
+    public function get_user_bookings($user_id){
         $this->db->select('*');
-        $query = $this->db->get_where('users_profile', array('friconn_id' => $friconn_id));
-        $result = $query->row_array();
-        $result['institution'] = $this->fn_model->get_user_institution($result['institution_id']);
-        $result['department'] = $this->fn_model->get_user_department($result['department_id']);
-        return $result;
-    }
-    public function get_users(){
-        $role_id = $this->fn_model->get_user_role_id('user');
+        $this->db->where(['user_id' => $user_id]);
+        $bookings = $this->db->order_by('id', "desc")->get('bookings')->result();
 
-        $this->db->select('*');
-        $profiles = $this->db->get_where('users',['role_id' => $role_id])->result();
-        foreach ($profiles as $profile) {
-            $profile->profile = $this->_get_user_details($profile->friconn_id);
-        }
-        return $profiles;
-    }
+        if ($bookings) {
+            foreach ($bookings as $booking) {
+                $booking = $this->fn_model->get_user_booking($booking->booking_id);
+                $booking->booking = $booking;
+            }
 
-    public function get_user($friconn_id){
-        $role_id = $this->fn_model->get_user_role_id('user');
-        $this->db->select('*');
-        $user = $this->db->get_where('users',['friconn_id' => $friconn_id,'role_id' => $role_id])->row_array();
-
-        if ($user['role_id'] == $role_id) {
-            $user_details = $this->_get_user_details($friconn_id);
-            return $user;
+            return $bookings;
         }
         return null;
     }
 
-    public function update_user_profile($data)
-    {
-        $update_profile = $this->db->update('users_profile',$data,['friconn_id' => $data['friconn_id']]);
-        if ($update_profile) {
+
+    public function add_user_booking($data){
+        if ($this->db->insert('user_bookings',$data)) {
             return true;
         }
         return false;
-    }
-
-    public function get_user_points($friconn_id){
-        $this->db->select('points');
-        $user_points = $this->db->get_where('users_profile',['friconn_id' => $friconn_id])->row_array();
-
-        if ($user_points) {
-            return $user_points['points'];
-        }
-        return null;
-    }
-
-    public function update_user_points($data)
-    {
-        $update_points = $this->db->update('users_profile',$data,['friconn_id' => $data['friconn_id']]);
-        if ($update_points) {
-            return $this->get_user_points($data['friconn_id']);
-        }
-        return false;
-    }
-
-    public function get_users_courses(){
-        $this->db->select('*');
-        $users_courses = $this->db->order_by('id', "desc")->get('user_courses')->result();
-
-        if ($users_courses) {
-            foreach ($users_courses as $users_course) {
-                $user = $this->fn_model->get_user_via_friconn_id($users_course->friconn_id);
-                $users_course->user = $user['last_name']. ' '.$user['other_names'];
-
-                $course = $this->fn_model->get_user_course($users_course->course_id);
-                $users_course->course = $course;
-            }
-
-            return $users_courses;
-        }
-        return null;
-    }
-
-    public function get_user_courses($friconn_id){
-        $this->db->select('*');
-        $this->db->where(['friconn_id' => $friconn_id]);
-        $user_courses = $this->db->order_by('id', "desc")->get('user_courses')->result();
-
-        if ($user_courses) {
-            foreach ($user_courses as $user_course) {
-                $user = $this->fn_model->get_user_via_friconn_id($user_course->friconn_id);
-                $user_course->user = $user['last_name']. ' '.$user['other_names'];
-
-                $course = $this->fn_model->get_user_course($user_course->course_id);
-                $user_course->course = $course;
-            }
-
-            return $user_courses;
-        }
-        return null;
-    }
-
-    public function check_user_course($data){
-        $this->db->select('id');
-        $this->db->where(['course_id' => $data['course_id'],'friconn_id' =>$data['friconn_id']]);
-        $users_course = $this->db->get('user_courses')->num_rows();
-        if ($users_course > 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public function add_user_course($data){
-        if ($this->db->insert('user_courses',$data)) {
-            $course = $this->get_user_last_course($data['friconn_id']);
-            return $course;
-        }
-        return false;
-    }
-
-    public function get_user_last_course($friconn_id){
-        $this->db->select('*');
-        $this->db->where(['friconn_id' => $friconn_id]);
-        $user_course = $this->db->order_by('id', "desc")->limit(1)->get('user_courses')->row_array();
-
-        if ($user_course) {
-            $course = $this->fn_model->get_user_course($user_course['course_id']);
-            $user_course['course'] = $course;
-
-            return $user_course;
-        }
-        return null;
-    }
-
-    public function get_user_questions($friconn_id){
-        $this->db->select('*');
-        $this->db->where(['friconn_id' => $friconn_id]);
-        $user_questions = $this->db->order_by('id', "desc")->get('questions')->result();
-
-        if ($user_questions) {
-            foreach ($user_questions as $user_question) {
-                $user = $this->fn_model->get_user_via_friconn_id($user_question->friconn_id);
-                $user_question->user = $user['last_name']. ' '.$user['other_names'];
-                $user_question->question = substr($user_question->question, 0,119);
-
-                $course = $this->fn_model->get_user_course($user_question->course_id);
-                $user_question->course = $course;
-            }
-
-            return $user_questions;
-        }
-        return null;
-    }
-
-    public function get_user_answers($friconn_id){
-        $this->db->select('*');
-        $this->db->where(['friconn_id' => $friconn_id]);
-        $user_answers = $this->db->order_by('id', "desc")->get('question_answers')->result();
-
-        if ($user_answers) {
-            foreach ($user_answers as $user_answer) {
-                $user = $this->fn_model->get_user_via_friconn_id($user_answer->friconn_id);
-                $user_answer->user = $user['last_name']. ' '.$user['other_names'];
-
-                $question = $this->fn_model->get_user_question($user_answer->question_id);
-                $user_answer->question = $question;
-            }
-
-            return $user_answers;
-        }
-        return null;
-    }
-
-    public function get_users_plans(){
-        $this->db->select('*');
-        $users_plans = $this->db->order_by('id', "desc")->get('user_plans')->result();
-
-        if ($users_plans) {
-            foreach ($users_plans as $users_plan) {
-                $user = $this->fn_model->get_user_via_friconn_id($users_plan->friconn_id);
-                $users_plan->user = $user['last_name']. ' '.$user['other_names'];
-
-                $plan = $this->fn_model->get_user_plan($users_plan->plan_id);
-                $users_plan->plan = $plan;
-            }
-
-            return $users_plans;
-        }
-        return null;
-    }
-
-    public function get_user_plans($friconn_id){
-        $this->db->select('*');
-        $this->db->where(['friconn_id' => $friconn_id]);
-        $user_plans = $this->db->order_by('id', "desc")->get('user_plans')->result();
-
-        if ($user_plans) {
-            foreach ($user_plans as $user_plan) {
-                $user = $this->fn_model->get_user_via_friconn_id($user_plan->friconn_id);
-                $user_plan->user = $user['last_name']. ' '.$user['other_names'];
-
-                $plan = $this->fn_model->get_user_plan($user_plan->plan_id);
-                $user_plan->plan = $plan;
-            }
-
-            return $user_plans;
-        }
-        return null;
-    }
-
-    public function check_user_plan($data){
-        $this->db->select('id');
-        $this->db->where(['plan_id' => $data['plan_id'],'friconn_id' =>$data['friconn_id']]);
-        $users_plan = $this->db->get('user_plans')->num_rows();
-        if ($users_plan > 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public function add_user_plan($data){
-        if ($this->db->insert('user_plans',$data)) {
-            $plan = $this->get_user_last_plan($data['friconn_id']);
-            return $plan;
-        }
-        return false;
-    }
-
-    public function get_user_last_plan($friconn_id){
-        $this->db->select('*');
-        $this->db->where(['friconn_id' => $friconn_id]);
-        $user_plan = $this->db->order_by('id', "desc")->limit(1)->get('user_plans')->row_array();
-
-        if ($user_plan) {
-            $plan = $this->fn_model->get_user_plan($user_plan['plan_id']);
-            $user_plan['plan'] = $plan;
-
-            return $user_plan;
-        }
-        return null;
-    }
-
-
-    public function get_user_payments($friconn_id){
-        $this->db->select('*');
-        $this->db->where(['friconn_id' => $friconn_id]);
-        $user_payments = $this->db->order_by('id', "desc")->get('payments')->result();
-
-        if ($user_payments) {
-            foreach ($user_payments as $user_payment) {
-                $user = $this->fn_model->get_user_via_friconn_id($user_payment->friconn_id);
-                $user_payment->user = $user['last_name']. ' '.$user['other_names'];
-                
-                $plan = $this->fn_model->get_user_plan($user_payment->plan_id);
-                $user_payment->plan = $plan;
-            }
-
-            return $user_payments;
-        }
-        return null;
     }
 }
